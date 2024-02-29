9a9f691c932b225257066bdb03d23ac62bea0dfa
StellarLane <152025819+StellarLane@users.noreply.github.com>
2024-02-29 12:29:11 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,6 +5,12 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
+        if (n%2==1)
+        {
+            return n*3+1;
+        }
+        else return n/2;
+        /*
         if (n  == 128) {
             return 1;
         } else if (n == 5) {
@@ -12,6 +18,8 @@ public class Collatz {
         } else {
             return n * 2;
         }
+
+         */
     }
 
     public static void main(String[] args) {
